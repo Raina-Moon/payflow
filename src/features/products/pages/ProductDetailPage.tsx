66eeca03883bdc09030e60f9e1c03f1588248bb7
Raina Moon <mds6425@gmail.com>
2025-08-22 16:56:48 +0900

    feat : 제품 상세 페이지 생성성

--- src/features/products/pages/ProductDetailPage.tsx
+++ src/features/products/pages/ProductDetailPage.tsx
@@ -1,9 +1,22 @@
-import React from 'react'
+import React from "react";
+import { useParams } from "react-router-dom";
+import { useProductQuery } from "../hooks";
+import { formatPrice } from "../../../shared/lib/price";
 
 const ProductDetailPage = () => {
+  const { id } = useParams();
+  const { data: pd, isLoading, isError } = useProductQuery(Number(id));
+  if (isLoading) return <div>로딩중...</div>;
+  if (isError) return <div>데이터를 불러오는 도중 오류가 발생했습니다.</div>;
+  if (!pd) return <div>상품을 찾을 수 없습니다.</div>;
   return (
-    <div>ProductDetailPage</div>
-  )
-}
+    <div>
+      <img src={pd?.image} alt={`${pd?.name}의 이미지`} />
+      <p>{pd?.name}</p>
+      <p>{pd?.category}</p>
+      <p>{formatPrice(pd?.price)}</p>
+    </div>
+  );
+};
 
-export default ProductDetailPage
+export default ProductDetailPage;
