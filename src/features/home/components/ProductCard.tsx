a48510210d9ad06197656601e56955db42ab7729
Raina Moon <mds6425@gmail.com>
2025-08-22 16:36:48 +0900

    ui : 홈페이지, 제품 카드 ui 1차 구현

--- src/features/home/components/ProductCard.tsx
+++ src/features/home/components/ProductCard.tsx
@@ -1,25 +1,75 @@
 import React from "react";
 import { useProductsQuery } from "../../products/hooks";
+import styled from "styled-components";
+import { Link, useNavigate } from "react-router-dom";
 
 const ProductCard = () => {
   const { data: product, isLoading, isError } = useProductsQuery();
+  const navigate = useNavigate();
   if (isLoading) return <div>로딩중...</div>;
   if (isError) return <div>데이터를 불러오는 도중 오류가 발생했습니다.</div>;
+
   return (
     <div>
-      <li>
+      <CardList>
         {product?.map((i) => (
-          <ul key={i.id}>
+          <Card key={i.id}>
             <img src={i.image} alt={i.name} />
             <p>{i.name}</p>
             <p>{i.price}</p>
             <p>{i.category}</p>
-            <button>상세보기</button>
-          </ul>
+            <button onClick={() => navigate(`/products/${i.id}`)}>
+              상세보기
+            </button>
+          </Card>
         ))}
-      </li>
+      </CardList>
     </div>
   );
 };
 
 export default ProductCard;
+
+const CardList = styled.li`
+  display: flex;
+  flex-wrap: wrap;
+  gap: 20px;
+`;
+
+const Card = styled.ul`
+  width: 200px;
+  height: 300px;
+  border: 1px solid #ccc;
+  border-radius: 10px;
+  padding: 10px;
+  text-align: center;
+  overflow: hidden;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+
+  img {
+    width: 100%;
+    max-height: 150px;
+    height: auto;
+    border-radius: 10px;
+  }
+
+  p {
+    margin: 5px 0;
+  }
+
+  button {
+    margin-top: 10px;
+    padding: 5px 10px;
+    background-color: #007bff;
+    color: white;
+    border: none;
+    border-radius: 5px;
+    cursor: pointer;
+
+    &:hover {
+      background-color: #0056b3;
+    }
+  }
+`;
--- src/features/home/pages/HomePage.tsx
+++ src/features/home/pages/HomePage.tsx
@@ -1,14 +1,22 @@
 import React from "react";
 import ProductCard from "../components/ProductCard";
+import { useUsersQuery } from "../../user/hooks";
+import { useNavigate } from "react-router-dom";
 
 const HomePage = () => {
+  const { data: users } = useUsersQuery();
+  const navigate = useNavigate();
+  const totalUsers = users?.length || 0;
+  const activeUsers = users?.filter((user) => user.active).length || 0;
+
   return (
     <div>
       <div>
-        <p>총 사용자 수</p>
-        <p>활성 사용자 수</p>
+        <p>총 사용자 수 : {totalUsers}명</p>
+        <button onClick={() => navigate(`/users`)}>사용자 리스트 보기</button>
+        <p>활성 사용자 수 : {activeUsers}명</p>
         <p>오늘 결제 성공 건수</p>
-        <p>오늘 결제 금액 합계계</p>
+        <p>오늘 결제 금액 합계</p>
         <p>최근 7일 결제 금액 차트</p>
         <p>TOP 5 구매자(사용자명/총 결제액)</p>
       </div>
