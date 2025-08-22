702d8e65bf6e5b782bf3adb2245b7cab18ec90e0
Raina Moon <mds6425@gmail.com>
2025-08-22 14:55:17 +0900

    refactor : app 폴더 디렉토리 수정정

--- src/app/routes/router.tsx
+++ src/app/routes/router.tsx
@@ -1,5 +1,5 @@
 import { createBrowserRouter } from "react-router-dom";
-import Header from "../../components/Header";
+import Header from "../components/Header";
 import HomePage from "../../features/home/pages/HomePage";
 import UsersListPage from "../../features/user/pages/UsersListPage";
 import UserDetailPage from "../../features/user/pages/UserDetailPage";
